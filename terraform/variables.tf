82c44e4bbf93cc3596f7d9bb0eac4253cedee06a
YairRodriguez96 <64326986+g0rducci@users.noreply.github.com>
2023-06-23 13:43:05 -0600

    add doc var.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,15 @@
 variable "region" {
-    default = "us-east-1"
+    default = "us-east-1"  # Variable que especifica la región de AWS, con un valor predeterminado de "us-east-1"
 }
+
 variable "public_key" {
-  
+  # Variable para especificar la clave pública utilizada para la conexión SSH a la instancia EC2
 }
+
 variable "private_key" {
-  
+  # Variable para especificar la clave privada utilizada para la conexión SSH a la instancia EC2
 }
+
 variable "key_name" {
-  
+  # Variable para especificar el nombre del par de claves utilizado para acceder a la instancia EC2
 }
